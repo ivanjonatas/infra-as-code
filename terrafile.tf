9c85f133a00061b2f93ba87447c4cece32f14502
ivan.jonatas <ivan.jonatas@conductor.com.br>
2021-04-16 00:02:54 -0300

    adicionando comando no ansible

--- terrafile.tf
+++ terrafile.tf
@@ -5,6 +5,6 @@ module "group" {
 module "instance" {
     source = "./ec2"
     # inst_sg = module.group.id_sg
-    inst_ami = "ami-0284e82bd57b42967"
+    inst_ami = "ami-0be5fa50a0e832128"
     inst_key = "k8s"
 }
